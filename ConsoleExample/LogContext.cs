46c8e366b17dbac76bd4e0e63149ab5d3dcc6bbd
Hakan Lindestaf <hakan@lindestaf.com>
2013-10-02 14:41:50 -0700

    NLog example code first

---
+++ ConsoleExample/LogContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLog;
+
+namespace Haukcode
+{
+    public static class Log
+    {
+        public static LogContext Context(Logger logger, string context)
+        {
+            return new LogContext(logger, context);
+        }
+    }
+
+    public class LogContext : IDisposable
+    {
+        private IDisposable ndc;
+        private Logger logger;
+        private System.Diagnostics.Stopwatch stopWatch;
+
+        public LogContext(Logger logger, string context)
+        {
+            this.logger = logger;
+
+            this.ndc = NestedDiagnosticsContext.Push(context);
+            this.stopWatch = System.Diagnostics.Stopwatch.StartNew();
+        }
+
+        public void Dispose()
+        {
+            this.stopWatch.Stop();
+
+            var logEvent = new LogEventInfo(
+                LogLevel.Info,
+                this.logger.Name,
+                string.Format("Duration {0:N1} ms", this.stopWatch.Elapsed.TotalMilliseconds));
+
+            logEvent.Properties["Duration"] = this.stopWatch.Elapsed.TotalMilliseconds.ToString("G1");
+            this.logger.Log(logEvent);
+
+            this.ndc.Dispose();
+        }
+    }
+}
---
+++ ConsoleExample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLog;
+
+namespace Haukcode.ConsoleApplication1
+{
+    public class Program
+    {
+        private static Logger log = LogManager.GetCurrentClassLogger();
+
+        public static void Main(string[] args)
+        {
+            log.Info("Basic logging");
+
+            using (Log.Context(log, "Main"))
+            {
+                log.Info("In context");
+
+                TestFunction("TestArg");
+            }
+
+            LogManager.Flush();
+
+            Console.WriteLine("Press ENTER to quit...");
+            Console.ReadLine();
+        }
+
+        private static void TestFunction(string arg1)
+        {
+            using (Log.Context(log, "TestFunction"))
+            {
+                log.Info("Argument {0}", arg1);
+
+                log.Warn("Warning caution!");
+
+                System.Threading.Thread.Sleep(1000);
+
+                log.Debug("Done...");
+            }
+        }
+    }
+}
